fbe5bf5b665bd095bddc2d50bfe5ff0f51ec917e
YuiYamada <ystv1212@gmail.com>
2023-12-23 07:23:13 +0000

    change config

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
+import twindPlugin from "$fresh/plugins/tailwind.ts";
+import twindConfig from "./tailwind.config.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [twindPlugin(twindConfig)],
 });
