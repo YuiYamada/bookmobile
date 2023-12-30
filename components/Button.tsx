bec15541f8b10cbb8cbbc11da972c6aa48f587a2
YuiYamada <ystv1212@gmail.com>
2023-12-30 17:17:00 +0000

    Fix TopPage Button&Icon

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,13 +5,15 @@ interface ButtonProps {
 
 export default function Button({ path, name }: ButtonProps) {
   return (
-    <a
-      href={path}
-      class="flex justify-center items-center bg-white rounded-full h-18 border-2 border-gray-300 transition-colors"
-    >
-      <span class="text-xl font-bold text-gray-900 group-hover:underline group-focus:underline">
-        {name}
-      </span>
-    </a>
+    <div class="flex flex-col items-center justify-start my-8">
+      <a
+        href={path}
+        class="flex justify-center items-center bg-white rounded-full w-2/3 h-16 border-2 border-gray-300 transition-colors"
+      >
+        <span class="text-xl font-bold text-gray-900 group-hover:underline group-focus:underline">
+          {name}
+        </span>
+      </a>
+    </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 import { Head } from "$fresh/runtime.ts";
 import Button from "../components/Button.tsx";
+import IconBooks from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/books.tsx";
 
 export default function index() {
   return (
@@ -12,8 +13,14 @@ export default function index() {
         />
       </Head>
       <main>
-        <Button path="/loginPage" name="Log In" />
+        <div class="flex flex-col items-center justify-start mt-60">
+          <IconBooks class="w-24 h-24" />
+          <p class="text-xl font-bold text-gray-900 group-hover:underline group-focus:underline">
+            bookmobile
+          </p>
+        </div>
         <Button path="/signUpPage" name="Sign Up" />
+        <Button path="/loginPage" name="Log In" />
       </main>
     </div>
   );
